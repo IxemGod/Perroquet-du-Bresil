2dbdae32fe76f840b1b55293b9519ae13121e2d0
ixemgod <ixemgod@gmail.com>
2024-03-22 11:44:20 +0100

    Optimisation du code

--- script.js
+++ script.js
@@ -1,17 +1,41 @@
-// const ListePlumePeroquet1 = 10;
-// const perroquets1 = document.querySelector('.green');
-// const plumes1 = perroquets1.querySelector('.plumes');
-// const mètreDeBière1 = perroquets1.querySelector('.mètre-de-bière1');
-// const verreDeBière1 = perroquets1.querySelector('.verre-de-bière1');
-// const nombreDePerroquetFini1 = perroquets1.querySelector('.nombre-de-perroquet-fini1');
-// const btnPlusmetre1 = perroquets1.querySelectorAll('.btnPlusmetre1');
-// const btnPlusVerre1 = perroquets1.querySelectorAll('.btnPlusVerre1');
-
-
 let plume1 = 8;
 let plume2 = 8;
 let plume3 = 8;
 
+
+
+function TestPlume(Plume, ChampsPlume){
+  if(Plume == 1){
+    ChampsPlume.innerHTML = "🪶";
+  }
+  if(Plume == 2){
+    ChampsPlume.innerHTML = "🪶🪶";
+  }
+  if(Plume == 3){
+    ChampsPlume.innerHTML = "🪶🪶🪶";
+  }
+  if(Plume == 4){
+    ChampsPlume.innerHTML = "🪶🪶🪶🪶";
+  }
+  if(Plume == 5){
+    ChampsPlume.innerHTML = "🪶🪶🪶🪶🪶";
+  }
+  if(Plume == 6){
+    ChampsPlume.innerHTML = "🪶🪶🪶🪶🪶🪶";
+  }
+  if(Plume == 7){
+    ChampsPlume.innerHTML = "🪶🪶🪶🪶🪶🪶🪶";
+  }
+  if(Plume == 8){
+    ChampsPlume.innerHTML = "🪶🪶🪶🪶🪶🪶🪶🪶";
+  }
+  if(Plume == 9){
+    ChampsPlume.innerHTML = "🪶🪶🪶🪶🪶🪶🪶🪶🪶";
+  }
+  if(Plume == 10){
+    ChampsPlume.innerHTML = "🪶🪶🪶🪶🪶🪶🪶🪶🪶🪶";
+  }
+}
 
 
 
@@ -29,98 +53,38 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
     btnPlusDuelGagn1.addEventListener('click', function() {
-      let currentValue = parseInt(DuelGanger1.value) + 1;
-      DuelGanger1.value = currentValue;
+    let currentValue = parseInt(DuelGanger1.value) + 1;
+    DuelGanger1.value = currentValue;
 
       if(currentValue%3 == 0)
       {
         plume1 = plume1 - 1;
         if((plume1) === 0)
         {
-            plume1 = 8;
-            let ValeurAcutelNbrPerroquetFini = parseInt(NbrPeroquetFini1.value);
-            NbrPeroquetFini1.value =  ValeurAcutelNbrPerroquetFini + 1;
+          plume1 = 8;
+          let ValeurAcutelNbrPerroquetFini = parseInt(NbrPeroquetFini1.value);
+          NbrPeroquetFini1.value =  ValeurAcutelNbrPerroquetFini + 1;
         }
-
-        if(plume1 == 1){
-          ChampsPlumes1.innerHTML = "🪶";
-        }
-        if(plume1 == 2){
-          ChampsPlumes1.innerHTML = "🪶🪶";
-        }
-        if(plume1 == 3){
-          ChampsPlumes1.innerHTML = "🪶🪶🪶";
-        }
-        if(plume1 == 4){
-          ChampsPlumes1.innerHTML = "🪶🪶🪶🪶";
-        }
-        if(plume1 == 5){
-          ChampsPlumes1.innerHTML = "🪶🪶🪶🪶🪶";
-        }
-        if(plume1 == 6){
-          ChampsPlumes1.innerHTML = "🪶🪶🪶🪶🪶🪶";
-        }
-        if(plume1 == 7){
-          ChampsPlumes1.innerHTML = "🪶🪶🪶🪶🪶🪶🪶";
-        }
-        if(plume1 == 8){
-          ChampsPlumes1.innerHTML = "🪶🪶🪶🪶🪶🪶🪶🪶";
-        }
-        if(plume1 == 9){
-          ChampsPlumes1.innerHTML = "🪶🪶🪶🪶🪶🪶🪶🪶🪶";
-        }
-        if(plume1 == 10){
-          ChampsPlumes1.innerHTML = "🪶🪶🪶🪶🪶🪶🪶🪶🪶🪶";
-        }
+        TestPlume(plume1, ChampsPlumes1);
       }
     });
 
 
 
     btnPlusDuelPerdu1.addEventListener('click', function() {
-      let currentValue = parseInt(DuelPerdu1.value) + 1;
-      DuelPerdu1.value = currentValue;
+    let currentValue = parseInt(DuelPerdu1.value) + 1;
+    DuelPerdu1.value = currentValue;
 
       if(currentValue%3 == 0)
       {
         plume1 = plume1 + 1;
         if((plume1) == 9)
         {
-            plume1 = 1;
-            let ValeurAcutelNbrPerroquetFini = parseInt(NbrPeroquetFini1.value);
-            NbrPeroquetFini1.value =  ValeurAcutelNbrPerroquetFini - 1;
+          plume1 = 1;
+          let ValeurAcutelNbrPerroquetFini = parseInt(NbrPeroquetFini1.value);
+          NbrPeroquetFini1.value =  ValeurAcutelNbrPerroquetFini - 1;
         }
-
-        if(plume1 == 1){
-          ChampsPlumes1.innerHTML = "🪶";
-        }
-        if(plume1 == 2){
-          ChampsPlumes1.innerHTML = "🪶🪶";
-        }
-        if(plume1 == 3){
-          ChampsPlumes1.innerHTML = "🪶🪶🪶";
-        }
-        if(plume1 == 4){
-          ChampsPlumes1.innerHTML = "🪶🪶🪶🪶";
-        }
-        if(plume1 == 5){
-          ChampsPlumes1.innerHTML = "🪶🪶🪶🪶🪶";
-        }
-        if(plume1 == 6){
-          ChampsPlumes1.innerHTML = "🪶🪶🪶🪶🪶🪶";
-        }
-        if(plume1 == 7){
-          ChampsPlumes1.innerHTML = "🪶🪶🪶🪶🪶🪶🪶";
-        }
-        if(plume1 == 8){
-          ChampsPlumes1.innerHTML = "🪶🪶🪶🪶🪶🪶🪶🪶";
-        }
-        if(plume1 == 9){
-          ChampsPlumes1.innerHTML = "🪶🪶🪶🪶🪶🪶🪶🪶🪶";
-        }
-        if(plume1 == 10){
-          ChampsPlumes1.innerHTML = "🪶🪶🪶🪶🪶🪶🪶🪶🪶🪶";
-        }
+        TestPlume(plume1, ChampsPlumes1);
       }
     });
 
@@ -134,91 +98,27 @@ document.addEventListener('DOMContentLoaded', function() {
         plume1 = plume1 - 1;
         if((plume1) === 0)
         {
-            plume1 = 8;
-            let ValeurAcutelNbrPerroquetFini = parseInt(NbrPeroquetFini1.value);
-            NbrPeroquetFini1.value =  ValeurAcutelNbrPerroquetFini + 1;
+          plume1 = 8;
+          let ValeurAcutelNbrPerroquetFini = parseInt(NbrPeroquetFini1.value);
+          NbrPeroquetFini1.value =  ValeurAcutelNbrPerroquetFini + 1;
         }
-
-        if(plume1 == 1){
-          ChampsPlumes1.innerHTML = "🪶";
-        }
-        if(plume1 == 2){
-          ChampsPlumes1.innerHTML = "🪶🪶";
-        }
-        if(plume1 == 3){
-          ChampsPlumes1.innerHTML = "🪶🪶🪶";
-        }
-        if(plume1 == 4){
-          ChampsPlumes1.innerHTML = "🪶🪶🪶🪶";
-        }
-        if(plume1 == 5){
-          ChampsPlumes1.innerHTML = "🪶🪶🪶🪶🪶";
-        }
-        if(plume1 == 6){
-          ChampsPlumes1.innerHTML = "🪶🪶🪶🪶🪶🪶";
-        }
-        if(plume1 == 7){
-          ChampsPlumes1.innerHTML = "🪶🪶🪶🪶🪶🪶🪶";
-        }
-        if(plume1 == 8){
-          ChampsPlumes1.innerHTML = "🪶🪶🪶🪶🪶🪶🪶🪶";
-        }
-        if(plume1 == 9){
-          ChampsPlumes1.innerHTML = "🪶🪶🪶🪶🪶🪶🪶🪶🪶";
-        }
-        if(plume1 == 10){
-          ChampsPlumes1.innerHTML = "🪶🪶🪶🪶🪶🪶🪶🪶🪶🪶";
-        }
-
+        TestPlume(plume1, ChampsPlumes1);
       }
-    //   if((currentValue+1)/12)
-
       // Si nombre est un multiple de 12, on enlêve une plumes
     });
   
     btnPlusmetre1.addEventListener('click', function() {
-      let currentValue = parseInt(mètreDeBière1.value);
-      mètreDeBière1.value = currentValue + 1;
+    let currentValue = parseInt(mètreDeBière1.value);
+    mètreDeBière1.value = currentValue + 1;
         plume1 = plume1 - 1;
 
         if((plume1) === 0)
         {
-            plume1 = 8;
-            let ValeurAcutelNbrPerroquetFini = parseInt(NbrPeroquetFini1.value);
-            NbrPeroquetFini1.value =  ValeurAcutelNbrPerroquetFini + 1;
+          plume1 = 8;
+          let ValeurAcutelNbrPerroquetFini = parseInt(NbrPeroquetFini1.value);
+          NbrPeroquetFini1.value =  ValeurAcutelNbrPerroquetFini + 1;
         }
-
-        if(plume1 == 1){
-          ChampsPlumes1.innerHTML = "🪶";
-        }
-        if(plume1 == 2){
-          ChampsPlumes1.innerHTML = "🪶🪶";
-        }
-        if(plume1 == 3){
-          ChampsPlumes1.innerHTML = "🪶🪶🪶";
-        }
-        if(plume1 == 4){
-          ChampsPlumes1.innerHTML = "🪶🪶🪶🪶";
-        }
-        if(plume1 == 5){
-          ChampsPlumes1.innerHTML = "🪶🪶🪶🪶🪶";
-        }
-        if(plume1 == 6){
-          ChampsPlumes1.innerHTML = "🪶🪶🪶🪶🪶🪶";
-        }
-        if(plume1 == 7){
-          ChampsPlumes1.innerHTML = "🪶🪶🪶🪶🪶🪶🪶";
-        }
-        if(plume1 == 8){
-          ChampsPlumes1.innerHTML = "🪶🪶🪶🪶🪶🪶🪶🪶";
-        }
-        if(plume1 == 9){
-          ChampsPlumes1.innerHTML = "🪶🪶🪶🪶🪶🪶🪶🪶🪶";
-        }
-        if(plume1 == 10){
-          ChampsPlumes1.innerHTML = "🪶🪶🪶🪶🪶🪶🪶🪶🪶🪶";
-        }
-
+        TestPlume(plume1, ChampsPlumes1);
       // Si on ajoute 1 litre, on retire un plumes
     });
   });
@@ -241,102 +141,40 @@ document.addEventListener('DOMContentLoaded', function() {
   const DuelPerdu2 = document.querySelector('.duelPerdu2');
 
   btnPlusDuelGagn2.addEventListener('click', function() {
-    let currentValue = parseInt(DuelGanger2.value) + 1;
-    DuelGanger2.value = currentValue;
+  let currentValue = parseInt(DuelGanger2.value) + 1;
+  DuelGanger2.value = currentValue;
 
     if(currentValue%3 == 0)
     {
       plume2 = plume2 - 1;
       if((plume2) === 0)
       {
-          plume2 = 8;
-          let ValeurAcutelNbrPerroquetFini = parseInt(NbrPeroquetFini2.value);
-          NbrPeroquetFini2.value =  ValeurAcutelNbrPerroquetFini + 1;
+        plume2 = 8;
+        let ValeurAcutelNbrPerroquetFini = parseInt(NbrPeroquetFini2.value);
+        NbrPeroquetFini2.value =  ValeurAcutelNbrPerroquetFini + 1;
       }
-
-      if(plume2 == 1){
-        ChampsPlumes2.innerHTML = "🪶";
-      }
-      if(plume2 == 2){
-        ChampsPlumes2.innerHTML = "🪶🪶";
-      }
-      if(plume2 == 3){
-        ChampsPlumes2.innerHTML = "🪶🪶🪶";
-      }
-      if(plume2 == 4){
-        ChampsPlumes2.innerHTML = "🪶🪶🪶🪶";
-      }
-      if(plume2 == 5){
-        ChampsPlumes2.innerHTML = "🪶🪶🪶🪶🪶";
-      }
-      if(plume2 == 6){
-        ChampsPlumes2.innerHTML = "🪶🪶🪶🪶🪶🪶";
-      }
-      if(plume2 == 7){
-        ChampsPlumes2.innerHTML = "🪶🪶🪶🪶🪶🪶🪶";
-      }
-      if(plume2 == 8){
-        ChampsPlumes2.innerHTML = "🪶🪶🪶🪶🪶🪶🪶🪶";
-      }
-      if(plume2 == 9){
-        ChampsPlumes2.innerHTML = "🪶🪶🪶🪶🪶🪶🪶🪶🪶";
-      }
-      if(plume2 == 10){
-        ChampsPlumes2.innerHTML = "🪶🪶🪶🪶🪶🪶🪶🪶🪶🪶";
-      }
+      TestPlume(plume2, ChampsPlumes2);
     }
   });
 
 
 
   btnPlusDuelPerdu2.addEventListener('click', function() {
-    let currentValue = parseInt(DuelPerdu2.value) + 1;
-    DuelPerdu2.value = currentValue;
+  let currentValue = parseInt(DuelPerdu2.value) + 1;
+  DuelPerdu2.value = currentValue;
 
     if(currentValue%3 == 0)
     {
       plume2 = plume2 + 1;
       if((plume2) == 9)
       {
-          plume2 = 1;
-          let ValeurAcutelNbrPerroquetFini = parseInt(NbrPeroquetFini2.value);
-          NbrPeroquetFini2.value =  ValeurAcutelNbrPerroquetFini - 1;
+        plume2 = 1;
+        let ValeurAcutelNbrPerroquetFini = parseInt(NbrPeroquetFini2.value);
+        NbrPeroquetFini2.value =  ValeurAcutelNbrPerroquetFini - 1;
       }
-
-      if(plume2 == 1){
-        ChampsPlumes2.innerHTML = "🪶";
-      }
-      if(plume2 == 2){
-        ChampsPlumes2.innerHTML = "🪶🪶";
-      }
-      if(plume2 == 3){
-        ChampsPlumes2.innerHTML = "🪶🪶🪶";
-      }
-      if(plume2 == 4){
-        ChampsPlumes2.innerHTML = "🪶🪶🪶🪶";
-      }
-      if(plume2 == 5){
-        ChampsPlumes2.innerHTML = "🪶🪶🪶🪶🪶";
-      }
-      if(plume2 == 6){
-        ChampsPlumes2.innerHTML = "🪶🪶🪶🪶🪶🪶";
-      }
-      if(plume2 == 7){
-        ChampsPlumes2.innerHTML = "🪶🪶🪶🪶🪶🪶🪶";
-      }
-      if(plume2 == 8){
-        ChampsPlumes2.innerHTML = "🪶🪶🪶🪶🪶🪶🪶🪶";
-      }
-      if(plume2 == 9){
-        ChampsPlumes2.innerHTML = "🪶🪶🪶🪶🪶🪶🪶🪶🪶";
-      }
-      if(plume2 == 10){
-        ChampsPlumes2.innerHTML = "🪶🪶🪶🪶🪶🪶🪶🪶🪶🪶";
-      }
+      TestPlume(plume2, ChampsPlumes2);
     }
   });
-
-
   btnPlusVerre2.addEventListener('click', function() {
     let currentValue2 = parseInt(verreDeBière2.value);
     verreDeBière2.value = currentValue2 + 1;
@@ -344,44 +182,12 @@ document.addEventListener('DOMContentLoaded', function() {
       plume2 = plume2 - 1;
       if((plume2) === 0)
       {
-          plume2 = 8;
-          let ValeurAcutelNbrPerroquetFini2 = parseInt(NbrPeroquetFini2.value);
-          NbrPeroquetFini2.value =  ValeurAcutelNbrPerroquetFini2 + 1;
+        plume2 = 8;
+        let ValeurAcutelNbrPerroquetFini2 = parseInt(NbrPeroquetFini2.value);
+        NbrPeroquetFini2.value =  ValeurAcutelNbrPerroquetFini2 + 1;
       }
-
-      if(plume2 == 1){
-        ChampsPlumes2.innerHTML = "🪶";
-      }
-      if(plume2 == 2){
-        ChampsPlumes2.innerHTML = "🪶🪶";
-      }
-      if(plume2 == 3){
-        ChampsPlumes2.innerHTML = "🪶🪶🪶";
-      }
-      if(plume2 == 4){
-        ChampsPlumes2.innerHTML = "🪶🪶🪶🪶";
-      }
-      if(plume2 == 5){
-        ChampsPlumes2.innerHTML = "🪶🪶🪶🪶🪶";
-      }
-      if(plume2 == 6){
-        ChampsPlumes2.innerHTML = "🪶🪶🪶🪶🪶🪶";
-      }
-      if(plume2 == 7){
-        ChampsPlumes2.innerHTML = "🪶🪶🪶🪶🪶🪶🪶";
-      }
-      if(plume2 == 8){
-        ChampsPlumes2.innerHTML = "🪶🪶🪶🪶🪶🪶🪶🪶";
-      }
-      if(plume2 == 9){
-        ChampsPlumes2.innerHTML = "🪶🪶🪶🪶🪶🪶🪶🪶🪶";
-      }
-      if(plume2 == 10){
-        ChampsPlumes2.innerHTML = "🪶🪶🪶🪶🪶🪶🪶🪶🪶🪶";
-      }
+      TestPlume(plume2, ChampsPlumes2);
     }
-  //   if((currentValue+1)/12)
-
     // Si nombre est un multiple de 12, on enlêve une plumes
   });
 
@@ -389,50 +195,17 @@ document.addEventListener('DOMContentLoaded', function() {
     let currentValue2 = parseInt(mètreDeBière2.value);
     mètreDeBière2.value = currentValue2 + 1;
       plume2 = plume2 - 1;
-
       if((plume2) === 0)
       {
         plume2 = 8;
-          let ValeurAcutelNbrPerroquetFini2 = parseInt(NbrPeroquetFini2.value);
-          NbrPeroquetFini2.value =  ValeurAcutelNbrPerroquetFini2 + 1;
+        let ValeurAcutelNbrPerroquetFini2 = parseInt(NbrPeroquetFini2.value);
+        NbrPeroquetFini2.value =  ValeurAcutelNbrPerroquetFini2 + 1;
       }
-
-      if(plume2 == 1){
-        ChampsPlumes2.innerHTML = "🪶";
-      }
-      if(plume2 == 2){
-        ChampsPlumes2.innerHTML = "🪶🪶";
-      }
-      if(plume2 == 3){
-        ChampsPlumes2.innerHTML = "🪶🪶🪶";
-      }
-      if(plume2 == 4){
-        ChampsPlumes2.innerHTML = "🪶🪶🪶🪶";
-      }
-      if(plume2 == 5){
-        ChampsPlumes2.innerHTML = "🪶🪶🪶🪶🪶";
-      }
-      if(plume2 == 6){
-        ChampsPlumes2.innerHTML = "🪶🪶🪶🪶🪶🪶";
-      }
-      if(plume2 == 7){
-        ChampsPlumes2.innerHTML = "🪶🪶🪶🪶🪶🪶🪶";
-      }
-      if(plume2 == 8){
-        ChampsPlumes2.innerHTML = "🪶🪶🪶🪶🪶🪶🪶🪶";
-      }
-      if(plume2 == 9){
-        ChampsPlumes2.innerHTML = "🪶🪶🪶🪶🪶🪶🪶🪶🪶";
-      }
-      if(plume2 == 10){
-        ChampsPlumes2.innerHTML = "🪶🪶🪶🪶🪶🪶🪶🪶🪶🪶";
-      }
-
+      TestPlume(plume2, ChampsPlumes2);
     // Si on ajoute 1 litre, on retire un plumes
   });
 });
   
-
 document.addEventListener('DOMContentLoaded', function() {
   const btnPlusVerre3 = document.querySelector('.btnPlusVerre3');
   const btnPlusmetre3 = document.querySelector('.btnPlusmetre3');
@@ -458,37 +231,7 @@ document.addEventListener('DOMContentLoaded', function() {
           let ValeurAcutelNbrPerroquetFini = parseInt(NbrPeroquetFini3.value);
           NbrPeroquetFini3.value =  ValeurAcutelNbrPerroquetFini + 1;
       }
-
-      if(plume3 == 1){
-        ChampsPlumes3.innerHTML = "🪶";
-      }
-      if(plume3 == 2){
-        ChampsPlumes3.innerHTML = "🪶🪶";
-      }
-      if(plume3 == 3){
-        ChampsPlumes3.innerHTML = "🪶🪶🪶";
-      }
-      if(plume3 == 4){
-        ChampsPlumes3.innerHTML = "🪶🪶🪶🪶";
-      }
-      if(plume3 == 5){
-        ChampsPlumes3.innerHTML = "🪶🪶🪶🪶🪶";
-      }
-      if(plume3 == 6){
-        ChampsPlumes3.innerHTML = "🪶🪶🪶🪶🪶🪶";
-      }
-      if(plume3 == 7){
-        ChampsPlumes3.innerHTML = "🪶🪶🪶🪶🪶🪶🪶";
-      }
-      if(plume3 == 8){
-        ChampsPlumes3.innerHTML = "🪶🪶🪶🪶🪶🪶🪶🪶";
-      }
-      if(plume3 == 9){
-        ChampsPlumes3.innerHTML = "🪶🪶🪶🪶🪶🪶🪶🪶🪶";
-      }
-      if(plume3 == 10){
-        ChampsPlumes3.innerHTML = "🪶🪶🪶🪶🪶🪶🪶🪶🪶🪶";
-      }
+      TestPlume(plume3, ChampsPlumes3);
     }
   });
 
@@ -508,36 +251,7 @@ document.addEventListener('DOMContentLoaded', function() {
           NbrPeroquetFini3.value =  ValeurAcutelNbrPerroquetFini - 1;
       }
 
-      if(plume3 == 1){
-        ChampsPlumes3.innerHTML = "🪶";
-      }
-      if(plume3 == 2){
-        ChampsPlumes3.innerHTML = "🪶🪶";
-      }
-      if(plume3 == 3){
-        ChampsPlumes3.innerHTML = "🪶🪶🪶";
-      }
-      if(plume3 == 4){
-        ChampsPlumes3.innerHTML = "🪶🪶🪶🪶";
-      }
-      if(plume3 == 5){
-        ChampsPlumes3.innerHTML = "🪶🪶🪶🪶🪶";
-      }
-      if(plume3 == 6){
-        ChampsPlumes3.innerHTML = "🪶🪶🪶🪶🪶🪶";
-      }
-      if(plume3 == 7){
-        ChampsPlumes3.innerHTML = "🪶🪶🪶🪶🪶🪶🪶";
-      }
-      if(plume3 == 8){
-        ChampsPlumes3.innerHTML = "🪶🪶🪶🪶🪶🪶🪶🪶";
-      }
-      if(plume3 == 9){
-        ChampsPlumes3.innerHTML = "🪶🪶🪶🪶🪶🪶🪶🪶🪶";
-      }
-      if(plume3 == 10){
-        ChampsPlumes3.innerHTML = "🪶🪶🪶🪶🪶🪶🪶🪶🪶🪶";
-      }
+      TestPlume(plume3, ChampsPlumes3);
     }
   });
 
@@ -554,40 +268,9 @@ document.addEventListener('DOMContentLoaded', function() {
           NbrPeroquetFini3.value =  ValeurAcutelNbrPerroquetFini3 + 1;
       }
 
-      if(plume3 == 1){
-        ChampsPlumes3.innerHTML = "🪶";
-      }
-      if(plume3 == 2){
-        ChampsPlumes3.innerHTML = "🪶🪶";
-      }
-      if(plume3 == 3){
-        ChampsPlumes3.innerHTML = "🪶🪶🪶";
-      }
-      if(plume3 == 4){
-        ChampsPlumes3.innerHTML = "🪶🪶🪶🪶";
-      }
-      if(plume3 == 5){
-        ChampsPlumes3.innerHTML = "🪶🪶🪶🪶🪶";
-      }
-      if(plume3 == 6){
-        ChampsPlumes3.innerHTML = "🪶🪶🪶🪶🪶🪶";
-      }
-      if(plume3 == 7){
-        ChampsPlumes3.innerHTML = "🪶🪶🪶🪶🪶🪶🪶";
-      }
-      if(plume3 == 8){
-        ChampsPlumes3.innerHTML = "🪶🪶🪶🪶🪶🪶🪶🪶";
-      }
-      if(plume3 == 9){
-        ChampsPlumes3.innerHTML = "🪶🪶🪶🪶🪶🪶🪶🪶🪶";
-      }
-      if(plume3 == 10){
-        ChampsPlumes3.innerHTML = "🪶🪶🪶🪶🪶🪶🪶🪶🪶🪶";
-      }
+      TestPlume(plume3, ChampsPlumes3);
 
     }
-  //   if((currentValue+1)/12)
-
     // Si nombre est un multiple de 12, on enlêve une plumes
   });
 
@@ -595,62 +278,13 @@ document.addEventListener('DOMContentLoaded', function() {
     let currentValue3 = parseInt(mètreDeBière3.value);
     mètreDeBière3.value = currentValue3 + 1;
       plume3 = plume3 - 1;
-
       if((plume3) === 0)
       {
         plume3 = 8;
           let ValeurAcutelNbrPerroquetFini3 = parseInt(NbrPeroquetFini3.value);
           NbrPeroquetFini3.value =  ValeurAcutelNbrPerroquetFini3 + 1;
       }
-
-      if(plume3 == 1){
-        ChampsPlumes3.innerHTML = "🪶";
-      }
-      if(plume3 == 2){
-        ChampsPlumes3.innerHTML = "🪶🪶";
-      }
-      if(plume3 == 3){
-        ChampsPlumes3.innerHTML = "🪶🪶🪶";
-      }
-      if(plume3 == 4){
-        ChampsPlumes3.innerHTML = "🪶🪶🪶🪶";
-      }
-      if(plume3 == 5){
-        ChampsPlumes3.innerHTML = "🪶🪶🪶🪶🪶";
-      }
-      if(plume3 == 6){
-        ChampsPlumes3.innerHTML = "🪶🪶🪶🪶🪶🪶";
-      }
-      if(plume3 == 7){
-        ChampsPlumes3.innerHTML = "🪶🪶🪶🪶🪶🪶🪶";
-      }
-      if(plume3 == 8){
-        ChampsPlumes3.innerHTML = "🪶🪶🪶🪶🪶🪶🪶🪶";
-      }
-      if(plume3 == 9){
-        ChampsPlumes3.innerHTML = "🪶🪶🪶🪶🪶🪶🪶🪶🪶";
-      }
-      if(plume3 == 10){
-        ChampsPlumes3.innerHTML = "🪶🪶🪶🪶🪶🪶🪶🪶🪶🪶";
-      }
-
+      TestPlume(plume3, ChampsPlumes3);
     // Si on ajoute 1 litre, on retire un plumes
   });
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
